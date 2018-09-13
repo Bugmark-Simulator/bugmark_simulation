cc602e4bf029c2ab556ed9aae587cebc379db205
Georg Link <glink@unomaha.edu>
2018-09-13 15:30:47 -0500

    remove unused data

--- data/python/Scripts/div7not5.py
+++
@@ -1,9 +0,0 @@
-# Question:
-# Write a Python program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
-# between 1000 and 3250 (both included).
-# The numbers obtained should be printed in a comma-separated sequence on a single line.
-#
-# Hints:
-# Consider use range(#begin, #end) method
-#
-# Solution:
